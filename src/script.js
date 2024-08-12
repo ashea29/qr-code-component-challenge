bf070fbb40751389019fcbf582a16cf446075152
Andrew Shea <andrew.shea783@gmail.com>
2024-08-11 20:00:52 -0600

    added theme toggle button, initial styles

--- src/script.js
+++ src/script.js
@@ -1,14 +1,14 @@
 import dtIconUrl from './images/dark-theme-icon.svg';
 import ltIconUrl from './images/light-theme-icon.svg';
 
-
-const themeToggle = document.querySelector('.theme-toggle');
-const toggleImg = document.querySelector('.theme-toggle img');
-
+// Get DOM elements
+const themeToggle = document.getElementById('theme-toggle');
+const toggleImg = document.querySelector('.theme-toggle__icon');
 
 // Attach event listeners
 themeToggle.addEventListener('click', () => {
   document.body.classList.toggle('dark-theme');
+  themeToggle.querySelector('span').textContent = document.body.classList.contains('dark-theme') ? 'Dark Theme' : 'Light Theme';
 
   toggleImg.src = document.body.classList.contains('dark-theme') ? dtIconUrl : ltIconUrl;
 });
