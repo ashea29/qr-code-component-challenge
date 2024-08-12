7d666481186d4842612e3cabac1e100bc9a562cd
Andrew Shea <andrew.shea783@gmail.com>
2024-08-12 08:57:02 -0600

    finished styles

--- src/script.js
+++ src/script.js
@@ -1,14 +1,25 @@
-import dtIconUrl from './images/dark-theme-icon.svg';
-import ltIconUrl from './images/light-theme-icon.svg';
+import dtIconUrl from "./images/dark-theme-icon.svg";
+import ltIconUrl from "./images/light-theme-icon.svg";
+import qrCodeDarkUrl from "./images/image-qr-code-dark.png";
+import qrCodeLightUrl from "./images/image-qr-code-light.png";
 
 // Get DOM elements
-const themeToggle = document.getElementById('theme-toggle');
-const toggleImg = document.querySelector('.theme-toggle__icon');
+const themeToggle = document.getElementById("theme-toggle");
+const toggleImg = document.getElementById("theme-icon");
+const qrCode = document.getElementById("qr-code");
 
 // Attach event listeners
-themeToggle.addEventListener('click', () => {
-  document.body.classList.toggle('dark-theme');
-  themeToggle.querySelector('span').textContent = document.body.classList.contains('dark-theme') ? 'Dark Theme' : 'Light Theme';
+themeToggle.addEventListener("click", () => {
+  document.body.classList.toggle("dark-theme");
+  themeToggle.querySelector("span").textContent =
+    document.body.classList.contains("dark-theme")
+      ? "Switch to light theme"
+      : "Switch to dark theme";
 
-  toggleImg.src = document.body.classList.contains('dark-theme') ? dtIconUrl : ltIconUrl;
+  qrCode.src = document.body.classList.contains("dark-theme")
+    ? qrCodeDarkUrl
+    : qrCodeLightUrl;
+  toggleImg.src = document.body.classList.contains("dark-theme")
+    ? ltIconUrl
+    : dtIconUrl;
 });
